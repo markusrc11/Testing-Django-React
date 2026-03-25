25fcdfe7b5ddfdfb57e423774a7cd5db5fc3bc5d
markusrc11 <markusrc11@gmail.com>
2026-03-25 20:43:10 +0100

    Add DRF for products

--- fase1/inventario/urls.py
+++ fase1/inventario/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.productos, name='lista_productos'),
-    path('nuevo/', views.crear_producto, name='crear_producto')
+    path('nuevo/', views.crear_producto, name='crear_producto'),
+    path('api/productos/', views.product_api_list, name='producto_api_list'),
+    path('api/productos/<int:pk>/', views.producto_api_detail, name='producto_api_detail'),
 ]
--- fase1/inventario/views.py
+++ fase1/inventario/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render, redirect
 from .models import Producto
 from .forms import ProductoForm
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import ProductoSerializer
 
 # Create your views here.
 def  productos(request):
   items = Producto.objects.all()
   return render(request, 'inventario/list.html', {'items': items})
+
+@api_view(['GET'])
+def product_api_list(request):
+  productos = Producto.objects.all()
+  serializer = ProductoSerializer(productos, many=True)
+  return Response(serializer.data)
+
+@api_view(['GET'])
+def producto_api_detail(request, pk):
+  try:
+    producto = Producto.objects.get(pk=pk)
+  except Producto.DoesNotExist:
+    return Response(status=404)
+
+  serializer = ProductoSerializer(producto)
+  return Response(serializer.data)
 
 def crear_producto(request):
   if request.method == 'POST':
