2fe22600fdf7d1a496c2ed2f7a54f06a08aa992c
markusrc11 <markusrc11@gmail.com>
2026-03-26 19:24:58 +0100

    Add POST request and CORS connection security

--- fase1/config/settings.py
+++ fase1/config/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'inventario',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -117,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# CORS settings
+CORS_ALLOW_ORIGINS = [
+  'http://localhost:3000',
+  'http://127.0.0.1:3000',
+]
--- fase1/inventario/views.py
+++ fase1/inventario/views.py
@@ -3,6 +3,7 @@ from .models import Producto
 from .forms import ProductoForm
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from .serializers import ProductoSerializer
 
 # Create your views here.
@@ -10,11 +11,19 @@ def  productos(request):
   items = Producto.objects.all()
   return render(request, 'inventario/list.html', {'items': items})
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def product_api_list(request):
-  productos = Producto.objects.all()
-  serializer = ProductoSerializer(productos, many=True)
-  return Response(serializer.data)
+  if request.method == 'GET':
+    productos = Producto.objects.all()
+    serializer = ProductoSerializer(productos, many=True)
+    return Response(serializer.data)
+  elif request.method == 'POST':
+    serializer = ProductoSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET'])
 def producto_api_detail(request, pk):
